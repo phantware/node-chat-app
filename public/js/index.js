a1d5bfc97ace15e291425b6a9ec754c5107fb51a
phantware <phantmoney2011@gmail.com>
2020-06-24 15:23:51 +0100

    communication between two clients

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,11 @@
 const socket = io();
 socket.on('connect', function () {
   console.log('Connected to the server');
-  socket.emit('createEmail', {
-    to: 'jainjanet@gmail.com',
-    text: 'Baba how far, I no hear from you na',
-  });
 });
 socket.on('disconnect', function () {
   console.log('server disconnected');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('New email sent', email);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
