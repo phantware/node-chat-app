c78a3a19abaaf896c70d90d9ea048e3170a5bf95
phantware <phantmoney2011@gmail.com>
2020-06-24 16:07:00 +0100

    communication between two clients

--- server/server.js
+++ server/server.js
@@ -23,11 +23,31 @@ io.on('connection', function (socket) {
   socket.on('createMessage', function (message) {
     console.log('createMessage', message);
 
+    // Admin welcome new user
+    socket.emit('newMessage', {
+      from: 'Admin',
+      message: 'Welcome to the chat app',
+      createdAt: new Date().getTime(),
+    });
+
+    // Admin send message to all users about the new user that joined
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      message: 'New user joined',
+      createdAt: new Date().getTime(),
+    });
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime(),
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime(),
+    // });
   });
 
   socket.on('disconnect', function () {
